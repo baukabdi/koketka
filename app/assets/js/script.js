a59e1ebe0c749c81cc0714f95cd48702bd0b12c6
Bauka <bauka.abdi@gmail.com>
2020-03-04 14:40:42 +0600

    done main responsive

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
 
+	var headerRight = $('.header_right');
+	$('.hamburger_button').on('click', function() {		
+		headerRight.addClass('header_is_active');
+	});
+
+	$('.times_button').on('click', function() {
+		headerRight.removeClass('header_is_active');
+	});
+
+	$('.search_button').on('click', function() {
+		var search = $('.header_search_wrap');
+
+
+		if (!search.is(':visible')) {
+			search.slideDown('fast');
+		}
+		else {
+			search.slideUp('fast');
+		}
+	});
+
+	$('.menu-item-has-children').on('click', function() {
+		var submenu = $(this).find('.sub-menu');
+		if (!submenu.is(':visible')) {
+			submenu.slideDown(500);
+		}
+		else {
+			submenu.slideUp(500);
+		}
+	});
+
 	/*===== Submenu change image BEGIN =====*/
 	$('.sub_list a').hover(function() {
 		var path = $(this).data('path');
@@ -9,8 +40,11 @@ $(document).ready(function() {
 	/*===== Submenu change image END =====*/
 
 
+	/*===== Main banner slider BEGIN =====*/
 	$('.main_banner_slider').slick({
 		prevArrow: '<div class="banner_arrow_prev"><img src="assets/images/slide-prev.svg"></div>',
 		nextArrow: '<div class="banner_arrow_next"><img src="assets/images/slide-next.svg"></div>',
 	});
+	/*===== Main banner slider EMD =====*/
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('serve', function () {
 		server: {
 			baseDir: 'app',
 			},		
-		notify: true,
+		notify: false,
 		open: true,
 		tunnel: false,
 	});
